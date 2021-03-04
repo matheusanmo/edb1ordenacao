bd273a2537dd11ec3cccd18457d306575faea148
matheus anmo <matheusanmo@gmail.com>
2021-03-04 19:17:36 -0300

    funcao is_ascending

--- src/main.cpp
+++ src/main.cpp
@@ -23,12 +23,35 @@ unsigned int* new_random_array(int n) {
     return new_array;
 }
 
+
+/**
+ * ordenacao nao precisa ser estrita
+ */
+bool is_ascending(unsigned int* first, unsigned int* last) {
+    unsigned int last_num = *first++;
+    while (first != last) {
+        unsigned int new_num = *first++;
+        if (new_num < last_num) {
+            return false;
+        }
+        last_num = new_num;
+    }
+    return true;
+}
+
+void print_array(unsigned int* first, unsigned int* last) {
+    while (first != last)
+        cout << *first++ << endl;
+    return;
+}
+
 int main() {
     int array_size{};
     cin >> array_size;
     unsigned int* new_array = new_random_array(array_size);
-    for (int i = 0; i < array_size; i++)
-        cout << new_array[i] << endl;
+    print_array(new_array, &new_array[array_size]);
+    cout << is_ascending(&new_array[0], &new_array[array_size]);
+
     delete[] new_array;
     new_array = nullptr;
 }
